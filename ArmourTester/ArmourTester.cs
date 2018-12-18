7a1919aed9d4ee5308f171da41687ccef5118bef
Matias Perämäki <mazku91@gmail.com>
2018-12-18 19:51:27 +0200

    Yksikkötestiluokka lisätty ja testiajureita.

---
+++ ArmourTester/ArmourTester.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Testing2018;
+
+namespace ArmourTester
+{
+    [TestClass]
+    public class ArmourTester
+    {
+        [TestMethod]
+        public void TestLevel1()
+        {
+            Armour ar = new Armour("Jorma", "Kokkeli", 20, 2, 2);
+            int lvl = ar.getLevel();
+
+            if (lvl != 2) {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod]
+        public void TestSlot1()
+        {
+            Armour ar = new Armour("Jorma", "Kokkeli", 20, 2, 2);
+            int lvl = ar.getSlot();
+
+            if (lvl != 2)
+            {
+                Assert.Fail();
+            }
+        }
+    }
+}
